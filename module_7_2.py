ba89bb81ce80d152e1bbb212a8573267c574549b
KvalIvan <serga.vanya14@gmail.com>
2024-09-16 17:32:46 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,18 +1,14 @@
-from pprint import pprint
-
-
 def custom_write(file_name, strings):
     strings_positions = {}
     string_number = 0
     file = open(file_name, 'w', encoding='utf-8')
     for string in strings:
         string_number += 1
-        string_list = [string_number, file.tell()]
+        position = file.tell()
         file.write(f'{string}\n')
-        f'{string_list}, {string}'
-        strings_positions[(string_number, file.tell())] = string
+        strings_positions[(string_number, position)] = string
     file.close()
-    return pprint(strings_positions)
+    return strings_positions
 
 
 info = [
@@ -22,4 +18,6 @@ info = [
     'Спасибо!'
     ]
 
-custom_write('test.txt', info)
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
